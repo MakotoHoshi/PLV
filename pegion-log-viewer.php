3f185ac5a6cb8ec24b02a98f27a0a62e08121d9d
MakotoHoshi <hoshi@mrfusion.co.jp>
2017-08-17 11:39:09 +0900

    v-1.0.0
    PLV(Pegion Log Viewer)
    Pegionメールライブラリのログを取得するWPプラグインです。

---
+++ pegion-log-viewer.php
@@ -0,0 +1,59 @@
+<?php
+/*
+Plugin Name: Pegion Log Viewer
+Plugin URI: https://github.com/MakotoHoshi/
+Description: Pegionメールライブラリに搭載されたDBモードに対応するログビューワです。
+Version: 1.0.0
+Author: MakotoHoshi
+Author URI: https://github.com/MakotoHoshi/
+License: GPL2
+*/
+
+add_action("admin_menu", "PLV_menu_output");
+function PLV_input(){
+
+}
+function PLV_console(){
+	/*テーブル名を設定*/
+	$tb_name = "log";
+
+	global $wpdb;
+	$sql = "";
+	$sql .= "SELECT * FROM `".$tb_name."`";
+	$result = $wpdb->get_results($sql);
+	$string = "";
+	$string .= "<h1>Pegion Log Viewer</h1>";
+	$string .= '<table class="wp-list-table widefat fixed striped posts">';
+	$string .= '<tbody>';
+	foreach($result as $key=>$value){
+		$string .= '<tr class="mail_ov">';
+		$string .= '<th>'.$value->_id.'</th>';
+		$string .= '<td>'.$value->time.'</td>';
+		foreach($value as $k=>$v){
+			if($k == "email" || $k == "user"){
+				$string .= '<td>'.$v.'</td>';
+			}
+		}
+		$string .= '</tr>';
+		$string .= '<tr class="mail_txt">
+		<th colspan="4">
+		<div style="height: 100px; overflow-y: scroll;">';
+		foreach($value as $k=>$v){
+			if($k == "_id" || $k == "time"){
+				continue;
+			}
+			$string .= $v."<br>";
+		}
+		$string .= '</div>
+		</th>
+		</tr>';
+	}
+	$string .= '</tbody>';
+	$string .= '</table>';
+
+	echo $string;
+}
+function PLV_menu_output(){
+	add_menu_page("PLV", "PLV", "manage_options", "plv", "PLV_console", content_url()."/plugins/PLV/img/icon.png");
+}
+?>
